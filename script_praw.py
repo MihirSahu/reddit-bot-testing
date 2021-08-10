bf8665459ab9143cbfcceb3577b3444216acf3db
Mihir Sahu <2002mihir@gmail.com>
2021-08-09 22:41:10 -0500

    added discord.py

--- script_praw.py
+++ script_praw.py
@@ -1,17 +1,41 @@
+#praw reddit api wrapper with discord bot implementation
 import praw
-import json
+import discord
+from discord.ext import commands
 
-client_id = ''
-secret_key = ''
-username = '' 
-password = ''
-user_agent = ''
 
-reddit = praw.Reddit(client_id=client_id, client_secret=secret_key, password=password, username=username, user_agent=user_agent)
+client = commands.Bot(command_prefix = '!')
 
-sub = '' 
-title = ''
-text = ''
+# Function to submit post
+def submitPost(cid, key, username, password, agent, sub, title, text):
 
-subreddit = reddit.subreddit(sub)
-subreddit.submit(title, selftext=text)
+    client_id = cid
+    secret_key = key
+    username = username
+    password = password
+    user_agent = agent
+
+    reddit = praw.Reddit(client_id=client_id, client_secret=secret_key, password=password, username=username, user_agent=user_agent)
+
+    sub = sub
+    title = title
+    text = text
+
+    subreddit = reddit.subreddit(sub)
+    subreddit.submit(title, selftext=text)
+
+# Function to inform us about the discord login
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# Function to detect commands
+@client.event
+async def on_message(message):
+    if message.content[:12] == "REDDITsubmit":
+        submitPost()
+    
+    await client.process_commands(message)
+
+# Run discord bot with token
+client.run('discord bot token')
